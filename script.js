a847b6421cdcc884d0d466e97f3591544716682d
Webdevk28 <117263501+Webdevk28@users.noreply.github.com>
2024-01-23 13:37:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -30,42 +30,41 @@ console.log(currentDate);
 
 
 // =================================================================
-// random tekegran
-var telegram = [
-    'https://t.me/Nguyenminh132',
-    'https://t.me/Dangngockhanhhh',
-    'https://t.me/lylong1779',
-    'https://t.me/ngoclinh0972',
-    'https://t.me/daohuyen113',
-    'https://t.me/emtrangnehihi',
-    'https://t.me/suohu58',
-    'https://t.me/thuytrang6656',
-    'https://t.me/lenghinghi',
-    'https://t.me/thattuyet_999',
-    'https://t.me/thythy868',
-    'https://t.me/baonamhay88',
-    'https://t.me/huyvaobo',
-    'https://t.me/thanhpham19971',
-    'https://t.me/boosshoanganh',
-    'https://t.me/huynhhy9',
-    'https://t.me/mylovehtq',
-    'https://t.me/betram1123',
-    'https://t.me/zzmailyzz86'
-];
-
+// random telegram
 function randomTlg() {
+    var telegram = [
+        'https://t.me/Nguyenminh132',
+        'https://t.me/Dangngockhanhhh',
+        'https://t.me/lylong1779',
+        'https://t.me/ngoclinh0972',
+        'https://t.me/daohuyen113',
+        'https://t.me/emtrangnehihi',
+        'https://t.me/suohu58',
+        'https://t.me/thuytrang6656',
+        'https://t.me/lenghinghi',
+        'https://t.me/thattuyet_999',
+        'https://t.me/thythy868',
+        'https://t.me/baonamhay88',
+        'https://t.me/huyvaobo',
+        'https://t.me/thanhpham19971',
+        'https://t.me/boosshoanganh',
+        'https://t.me/huynhhy9',
+        'https://t.me/mylovehtq',
+        'https://t.me/betram1123',
+        'https://t.me/zzmailyzz86'
+    ];
     var i = parseInt(Math.random() * telegram.length);
     location.href = telegram[i];
 }
 
-//  random zalo
-var zalo = [
-    'https://zalo.me/g/wzlgcw201',
-    'https://zalo.me/g/mtwifk462',
-    'https://zalo.me/g/ojucae708'
-]
 
+//  random zalo
 function randomZalo() {
+    var zalo = [
+        'https://zalo.me/g/wzlgcw201',
+        'https://zalo.me/g/mtwifk462',
+        'https://zalo.me/g/ojucae708'
+    ]
     var i = parseInt(Math.random() * zalo.length);
     location.href = zalo[i];
 }
